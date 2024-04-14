247aea08c9d73f0b205ce8c9c6604bdee253fd27
Hlova Oleh <hlovaoleh0@gmail.com>
2024-04-14 22:27:12 +0300

    added explanations to the functions

--- src/router.py
+++ src/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
+from src.models import DBNode, DBWorkflow
 from src.schemas import (
     Workflow,
     WorkflowCreate,
@@ -16,93 +17,129 @@ from src.schemas import (
 )
 from src import crud
 from dependencies import get_db
+from src.utils import execute_workflow_logic
 
 router = APIRouter(tags=["workflow_management"])
 
 
 @router.post("/workflows/", response_model=Workflow)
-async def create_workflow(workflow: WorkflowCreate, db: Session = Depends(get_db)) -> Workflow:
+async def create_workflow(
+        workflow: WorkflowCreate,
+        db: Session = Depends(get_db)
+) -> DBWorkflow:
+    """Create a new workflow"""
     return crud.create_workflow(db=db, workflow=workflow)
 
 
 @router.get("/workflows/", response_model=list[Workflow])
-async def read_all_workflows(db: Session = Depends()) -> list[Workflow]:
+async def read_all_workflows(
+        db: Session = Depends(get_db)
+) -> list[DBWorkflow]:
+    """Get all workflows"""
     return crud.get_all_workflows(db=db)
 
 
 @router.get("/workflows/{workflow_id}/", response_model=Workflow)
-async def read_workflow(workflow_id: int, db: Session = get_db) -> Workflow:
+async def read_workflow(
+        workflow_id: int,
+        db: Session = Depends(get_db)
+) -> DBWorkflow:
+    """Get a specific workflow by id"""
     return crud.get_workflow(db=db, workflow_id=workflow_id)
 
 
-@router.put("workflows/{workflow_id}/", response_model=Workflow)
+@router.put("/workflows/{workflow_id}/", response_model=Workflow)
 async def update_workflow(
         workflow_id: int,
         workflow: WorkflowCreate,
         db: Session = Depends(get_db)
-) -> Workflow:
+) -> DBWorkflow:
+    """Update a workflow by id"""
     return crud.update_workflow(db=db, workflow_id=workflow_id, workflow=workflow)
 
 
 @router.delete("/workflows/{workflow_id}/", response_model=Workflow)
-async def delete_workflow(workflow_id: int, db: Session = Depends(get_db)) -> None:
+async def delete_workflow(workflow_id: int, db: Session = Depends(get_db)) -> DBWorkflow:
     return crud.delete_workflow(db=db, workflow_id=workflow_id)
 
 
 @router.post("/nodes/", response_model=Node)
 async def create_node(
-        node: NodeCreate, db: Session = Depends(get_db)
-) -> Node:
+        node: NodeCreate,
+        db: Session = Depends(get_db)
+) -> DBNode:
+    """Create a new node (Only for creating Start and End nodes)"""
     return crud.create_node(db=db, node=node)
 
 
 @router.get("/nodes/", response_model=list[Node])
-async def read_all_nodes(db: Session = Depends(get_db)) -> list[Node]:
+async def read_all_nodes(db: Session = Depends(get_db)) -> list[DBNode]:
+    """Get all nodes"""
     return crud.get_all_nodes(db=db)
 
 
 @router.get("/nodes/{node_id}/", response_model=Node)
-async def read_node(node_id: int, db: Session = Depends(get_db)) -> Node:
-    node = crud.get_node(db=db, node_id=node_id)
+async def read_node(node_id: int, db: Session = Depends(get_db)) -> DBNode:
+    """Get a specific node by id"""
+    node = crud.get_node_by_id(db=db, node_id=node_id)
     if node is None:
         raise HTTPException(status_code=404, detail="Node not found")
     return node
 
 
 @router.delete("/nodes/{node_id}/", response_model=Node)
-async def delete_node(node_id: int, db: Session = Depends(get_db)) -> None:
+async def delete_node(node_id: int, db: Session = Depends(get_db)) -> DBNode:
+    """Delete a specific node by id"""
     return crud.delete_node(db=db, node_id=node_id)
 
 
 @router.post("/message_nodes/", response_model=MessageNode)
 async def create_message_node(
-        message_node: MessageNodeCreate, db: Session = Depends(get_db)
-) -> MessageNode:
+        message_node: MessageNodeCreate,
+        db: Session = Depends(get_db)
+) -> DBNode:
+    """Create a new message node"""
     return crud.create_message_node(db=db, message_node=message_node)
 
 
 @router.put("/message_nodes/{node_id}/", response_model=MessageNode)
 async def update_message_node(
-        node_id: int, data: MessageNodeCreate, db: Session = Depends(get_db)
-) -> MessageNode:
-    return crud.update_node(db=db, node_id=node_id, data=data)
+        node_id: int, data: MessageNodeCreate,
+        db: Session = Depends(get_db)
+) -> DBNode:
+    """Update a message node"""
+    return crud.update_message_node(db=db, node_id=node_id, data=data)
 
 
 @router.post("/condition_nodes/", response_model=ConditionNode)
 async def create_condition_node(
         condition_node: ConditionNodeCreate,
         db: Session = Depends(get_db)
-) -> ConditionNode:
+) -> DBNode:
+    """Create a new condition node"""
     return crud.create_condition_node(db=db, condition_node=condition_node)
 
 
 @router.put("/condition_nodes/{node_id}/", response_model=ConditionNode)
 async def update_condition_node(
-        node_id: int, data: ConditionNodeCreate, db: Session = Depends(get_db)
-) -> ConditionNode:
-    return crud.update_node(db=db, node_id=node_id, data=data)
+        node_id: int, data: ConditionNodeCreate,
+        db: Session = Depends(get_db)
+) -> DBNode:
+    """Update a condition node"""
+    return crud.update_condition_node(db=db, node_id=node_id, data=data)
 
 
 @router.post("/edges/", response_model=Edge)
 async def create_edge(edge: EdgeCreate, db: Session = Depends(get_db)):
+    """Create a new edge between two nodes"""
     return crud.create_edge(db=db, edge=edge)
+
+
+@router.get("/workflows/{workflow_id}/execute/")
+async def execute_workflow(workflow_id: int, db: Session = Depends(get_db)) -> list:
+    """Creates a new graph,
+    adds all the nodes and edges to the graph
+    and looking for the shortest path"""
+    workflow = crud.get_workflow(db, workflow_id)
+
+    return execute_workflow_logic(workflow)
